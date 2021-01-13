7a9d4e42971aaa7d67a464b63b2c731752c12d08
Joe <joelihai2021@gmail.com>
2021-01-13 09:22:12 +0800

    save as csv.

--- products.py
+++ products.py
@@ -4,12 +4,12 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格: ')
-	# p = []
-	# p.append(name)
-	# p.append(price)
-	# p = [name, price]
 	products.append([name, price])
 print(products)
 
 for product in products:
 	print(product[0], '的價格是', product[1])
+
+with open ('products.csv', 'w') as f: # 存csv檔可以用EXCEL開啟，要用逗點隔開
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
